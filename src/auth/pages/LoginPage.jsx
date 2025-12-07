67a09fbf2842a5de706068331896e015d0c4a9b8
oaleman-arm <oscar.aleman2018@gmail.com>
2025-12-06 23:44:46 -0600

    Avances foodApp v6.0

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
+import {useNavigate} from "react-router-dom";
 
 export const LoginPage = () => {
     const navigate = useNavigate();
-
     const onLogin = () => {
         navigate('/', {
             replace: true
@@ -17,8 +17,10 @@ export const LoginPage = () => {
                 className="btn btn-primary"
                 onClick={ onLogin }
             >
-                Login
+                Ingresar
             </button>
+            <hr />
+            <h2>Esta pagina esta en construccion...</h2>
 
         </div>
     )
